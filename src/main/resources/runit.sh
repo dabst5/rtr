90f2c274096508d55b2d60fdf04a06aff4a66073
anon227 <dasanigg@gmail.com>
2020-02-17 12:54:07 -0500

    Improving documentation & addint runit.sh alt

--- src/main/resources/runit.sh
+++ src/main/resources/runit.sh
@@ -1,2 +1,5 @@
 docker build --rm -t cjonesy/docker-spark:2.4.4 ../../../
 
+#WORKDIR=../../../
+
+#spark-submit --master=local --class=com.clfanalyzer.driver.ClfAnalyzer --master spark://spark:7077 $WORKDIR/target/ClfAnalyzer-0.0.1-SNAPSHOT-jar-with-dependencies.jar 5
